5fe698214bb9fa87286abafabbbb42ff9ef0746b
Diego Rodrigues <diego17ar@hotmail.com>
2022-06-20 23:16:47 -0300

    Manipulação dos prints para que as primeiras letras sejam impressas Maiusculas

--- Python/Oritencao a Objeto/Atividade_Carta/carta.py
+++ Python/Oritencao a Objeto/Atividade_Carta/carta.py
@@ -1,8 +1,11 @@
+from turtle import title
+
+
 dados_carta = ["nome", "logradouro", "numero","complemento","bairro","cep","cidade","estado"]
 dados_usuario ={}
 
 for dado in dados_carta:
-    pergunta = "Por favor informe seu {}: ".format(dado).title()
+    pergunta = "Por favor informe seu {}: ".format(dado)
     dados_usuario[dado] = input(pergunta)
     #print(dados_usuario)
 
@@ -11,18 +14,18 @@ cep = '{}-{}'.format(traco_cep[:5], traco_cep[5:])
 dados_usuario['cep'] = cep
 
 carta = """
-{}
-{}, {} {}
-{}
-{} {}/{}"""
+{nome}
+{l}, {n} {c}
+{b}
+{cep} {cidade}/{uf}"""
 
 print(carta.format(
-    dados_usuario['nome'],
-    dados_usuario['logradouro'],
-    dados_usuario['numero'],
-    dados_usuario['complemento'],
-    dados_usuario['bairro'],
-    dados_usuario['cep'],
-    dados_usuario['cidade'],
-    dados_usuario['estado']
+    nome=dados_usuario['nome'].title(),
+    l=dados_usuario['logradouro'].title(),
+    n=dados_usuario['numero'],
+    c=dados_usuario['complemento'].title(),
+    b=dados_usuario['bairro'].title(),
+    cep=dados_usuario['cep'],
+    cidade=dados_usuario['cidade'].title(),
+    uf=dados_usuario['estado'].upper()
 ))
