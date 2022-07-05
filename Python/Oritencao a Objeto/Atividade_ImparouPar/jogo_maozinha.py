10a2942c1b7521dfe147ec4b336a329a53435980
Diego Rodrigues <22277907+dhrodrigues@users.noreply.github.com>
2022-07-04 23:00:17 -0300

    Atividade em desenvolvimento

---
+++ Python/Oritencao a Objeto/Atividade_ImparouPar/jogo_maozinha.py
@@ -0,0 +1,11 @@
+import random
+
+parorimpar = ""
+while parorimpar != "par" and parorimpar != "impar":
+    parorimpar = input("""Bora Jogar!!!\n Bem-Vindo ao decissão de impar ou par.
+    \n Escolhe ai você vai ser impar ou par?""").lower
+
+
+while
+
+ 
