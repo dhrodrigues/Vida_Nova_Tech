d7d9f4587f64b82dc5779ea3dfe4ec2b4f45a29c
Diego Rodrigues <22277907+dhrodrigues@users.noreply.github.com>
2022-07-05 23:12:10 -0300

    Atualização da pasta

--- Python/Oritencao a Objeto/Atividade_Boliche/boliche.py
+++ Python/Oritencao a Objeto/Atividade_Boliche/boliche.py
@@ -1,8 +1,3 @@
-# def player():
-#     global pista
-#     jogadas = int(input("\nInforme os pinos que deseja derrubar informe um numero de (1 á 10): "))
-#     pista.pop(jogadas)
-
 def mostrar_pista(pinos):
     for x in pista:
         print(x, end = '')
@@ -26,41 +21,9 @@ map_pinos = {
     "10": 6
 }
 
-
- 
-
-game =[]
+game = ["3","5","8"]
 
 for pino in game:
     posicao = map_pinos[pino]
     pista[posicao]=" "
 mostrar_pista(pista)
-# while True:
-#     mostrar_pista(pista)
-
-#     for i in range(0,10):
-#         x =int(input("Informe numeros de 1 á 10 para vermos os pinos cairem!!:"))
-#         posicao = map_pinos[i]
-#         pista[posicao]= " "
-#     for i in game:
-#         if x == i:
-#             print("Não tem esse pino broww!")
-#         else:
-#             game.append(x)
-        
-#     print(v)
-    
-
-# for pino, posicao in map_pinos.items():
-#     posicao = map_pinos[pino]
-#     pista[posicao]=pino
-#     #   print(pino,posicao)
-# mostrar_pista(pista)
-
-# while True:
-#     if "I" in pista:
-#         mostrar_pista(pista)
-#         player()
-#     else:
-#         print("Jogo encerrado")
-
