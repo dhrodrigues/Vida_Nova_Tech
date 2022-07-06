5f1d6b24552183b52df39bdac61f458532a96da4
Diego Rodrigues <22277907+dhrodrigues@users.noreply.github.com>
2022-07-05 23:13:06 -0300

    Adicionando Arquivo quitanda.py

---
+++ Python/Oritencao a Objeto/Atividade_Quitanda/quitanda.py
@@ -0,0 +1,14 @@
+
+frutas = ["Banana"," Laranja", "Kiwi"]
+
+precos =  ["3", "8", "15"]
+
+valores={
+    "Banana": 3,
+    "Laranja": 8,
+    "Kiwi":15
+}
+mensagem_quintanda = ""
+fruta = input("Escolha a fruta que deseja {fruta} ":.format[frutas]
+print(fruta)
+# print("O preço unitário dessa fruta é {}".format(valores[fruta]))
