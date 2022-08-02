90000c00d868fd7bf0383a10f74377bcc76994e1
Diego Rodrigues <diego.rodrigues@rgc.com.br>
2022-08-02 10:14:33 -0300

    ATUALIZAÇÃO DO CODIGO DA QUINTANDA

--- Python/Oritencao a Objeto/Atividade_Quitanda/quitanda.py
+++ Python/Oritencao a Objeto/Atividade_Quitanda/quitanda.py
@@ -1,14 +1,41 @@
+class Quitanda:
 
-frutas = ["Banana"," Laranja", "Kiwi"]
+    def __init__(self):
+        self.frutas = {
+            "1": "Laranja",
+            "2": "Melão",
+            "3": "Kiwi"
+        }
+        self.precos = {
+            "1": 3,
+            "2": 8,
+            "3": 15
+        }
+    
+    def pergunta_fruta(self):
+        print("Olá Cliente!!!\n Essas são as nossas frutas fresquinhas:")
+        for cod in self.frutas:
+            fruta = self.frutas[cod]
+            print (f"{cod} - {fruta}")
 
-precos =  ["3", "8", "15"]
+        user_cod = input("Informe o numero da fruta que deseja:")
+        user_fruta = self.frutas[user_cod]
+        print(f"Você escolheu {user_fruta}")
+    
+    def quantidade (self):
+        user_qtd = input(f"Quantas você deseja ?\n")
+        return int(user_qtd)
+    
+    def calculadora (self,cod, qtd):
+        preco_unit = self.precos[cod]
+        preco_total = preco_unit * qtd
+        return preco_total
+    
+    def pedido (self):
+        cod = self.pergunta_fruta()
+        qtd = self.quantidade()
+        preco_total = self.calculadora (cod, qtd)
+        print(f"Deu R$ {preco_total,},00")
 
-valores={
-    "Banana": 3,
-    "Laranja": 8,
-    "Kiwi":15
-}
-mensagem_quintanda = ""
-fruta = input("Escolha a fruta que deseja {fruta} ":.format[frutas]
-print(fruta)
-# print("O preço unitário dessa fruta é {}".format(valores[fruta]))
+teste = Quitanda()
+teste.pedido()
